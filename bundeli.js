7d2d0bc9e1623706fc76dd8cea2b041e031328dd
rodyrahi <rodyrahi126@gmail.com>
2023-09-19 15:30:12 +0530

    better

--- bundeli.js
+++ bundeli.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 const sharp = require('sharp');
-const imagemin = require('imagemin');
-const imageminSharp = require('imagemin-sharp');
+
 const {db , dbimage , dbkissan} = require('./db');
 const session = require("express-session");
 const FileStore = require('session-file-store')(session);
@@ -119,24 +118,18 @@ app.post('/create', upload.array('images', 5), async (req, res) => {
 
   // const imagePromises = [];
 
-  const images = await Promise.all(
-    (req.files || []).map(async (file) => {
-      const compressedImageBuffer = await imagemin.buffer(file.buffer, {
-        plugins: [
-          imageminSharp({
-            resize: { width: 800 },
-            webp: { quality: 10 },
-          }),
-        ],
-      });
+  const images = await Promise.map(req.files || [], async (file) => {
+    const compressedImageBuffer = await sharp(file.buffer)
+      .resize({ width: 800 })
+      .webp({ quality: 40 })
+      .toBuffer();
 
-      // if (compressedImageBuffer.length > 100000) {
-      //   throw new Error('Image size exceeds 100KB');
-      // }
+    if (compressedImageBuffer.length > 100000) {
+      throw new Error('Image size exceeds 100KB');
+    }
 
-      return compressedImageBuffer;
-    })
-  );
+    return compressedImageBuffer;
+  }, { concurrency: 4 }); 
 
   // const images = await Promise.all(imagePromises);
   
